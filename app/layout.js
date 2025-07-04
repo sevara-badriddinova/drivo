743c8693af072505953549bc93f3ac8a551986fd
sevbad <sbadriddinova@usf.edu>
2025-07-04 00:02:00 -0400

    prepare for deployment

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/_components/header";
-import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
 
 const inter = Inter({subsets:["latin"]});
